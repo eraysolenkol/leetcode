821b484d2326e484d9c509fb0377fffd2ff64598
Eray <95094747+eraysolenkol@users.noreply.github.com>
2022-07-01 14:58:01 +0300

    Create 1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java

---
+++ java/1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,20 @@
+/* Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array. */
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] > nums[j]) {
+                    count++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
